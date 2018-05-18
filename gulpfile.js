5a36c2eea421ceb0d67be827226ed80d967ca7a3
Geass <geass1000@gmail.com>
2018-05-18 09:43:26 +0300

    update: use gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -15,15 +15,15 @@ const tsProject = ts.createProject(`${folderConf}/tsconfig.app.json`);
 const tsTest = ts.createProject(`${folderConf}/tsconfig.spec.json`);
 
 gulp.task('clean:code', () => {
-    return gulp.src(folderApp, { read: false })
+    return gulp.src(folderApp, { read: false, allowEmpty: true })
         .pipe(clean({ force: true }));
 });
 gulp.task('clean:test', () => {
-    return gulp.src(folderTests, { read: false })
+    return gulp.src(folderTests, { read: false, allowEmpty: true })
         .pipe(clean({ force: true }));
 });
 
-gulp.task('build:prod', ['clean:code'], () => {
+gulp.task('build:prod', gulp.series(['clean:code'], () => {
     const tsResult = tsProject.src()
         .pipe(tsProject());
 
@@ -31,28 +31,33 @@ gulp.task('build:prod', ['clean:code'], () => {
         tsResult.dts.pipe(gulp.dest(folderApp)),
         tsResult.js.pipe(gulp.dest(folderApp)),
     ]);
-});
-gulp.task('build:code', ['clean:code'], () => {
+}));
+gulp.task('build:code', gulp.series(['clean:code'], () => {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest(folderApp));
-});
-gulp.task('build:test', ['clean:test'], () => {
+}));
+gulp.task('build:test', gulp.series(['clean:test'], () => {
     return tsTest.src()
         .pipe(tsTest())
         .js.pipe(gulp.dest(folderTests));
-});
+}));
 
-gulp.task('start:test:unit', () => {
+gulp.task('start:test:unit', (done) => {
     return gulp.src([ `${folderTests}/**/*.spec.js`, `!${folderTests}/thing-it-tests/**/*.spec.js` ])
         .pipe(mocha({
-            reporter: 'progress'
-        }));
+            reporter: 'progress',
+        }))
+        .on('error', () => {
+            try {
+                console.log(error);
+            } catch (error) { ; }
+        });
 });
 gulp.task('start:test:mock', () => {
     return gulp.src(`${folderTests}/thing-it-tests/**/*.spec.js`)
         .pipe(mocha({
-            reporter: 'progress'
+            reporter: 'progress',
         }));
 });
 
@@ -71,31 +76,25 @@ gulp.task('tslint:code', () =>
         .pipe(tslint.report())
 );
 
-gulp.task('watch:test:unit', [ 'tslint:test', 'build:test' ], () => {
-    runSequence('start:test:unit');
-    gulp.watch([ `${folderSrc}/**/*.ts` ], () => {
-        runSequence('tslint:test', 'build:test', 'start:test:unit');
-    });
-});
-gulp.task('watch:test:mock', [ 'tslint:test', 'build:test' ], () => {
-    runSequence('start:test:mock');
-    gulp.watch([ `${folderSrc}/**/*.ts` ], () => {
-        runSequence('tslint:test', 'build:test', 'start:test:mock');
-    });
-});
+gulp.task('watch:test:unit', gulp.series([ 'tslint:test', 'build:test' ], 'start:test:unit', () => {
+    gulp.watch([ `${folderSrc}/**/*.ts` ],
+        gulp.series('tslint:test', 'build:test', 'start:test:unit'));
+}));
+gulp.task('watch:test:mock', gulp.series([ 'tslint:test', 'build:test' ], 'start:test:mock', () => {
+    gulp.watch([ `${folderSrc}/**/*.ts` ],
+        gulp.series('tslint:test', 'build:test', 'start:test:mock'));
+}));
 
-gulp.task('watch:build:test', [ 'tslint:test', 'build:test' ], () => {
-    gulp.watch([ `${folderSrc}/**/*.ts` ], () => {
-        runSequence('tslint:test', 'build:test');
-    });
-});
+gulp.task('watch:build:test', gulp.series([ 'tslint:test', 'build:test' ], () => {
+    gulp.watch([ `${folderSrc}/**/*.ts` ],
+        gulp.series('tslint:test', 'build:test'));
+}));
 
-gulp.task('watch:build:code', [ 'tslint:code', 'build:code' ], () => {
+gulp.task('watch:build:code', gulp.series([ 'tslint:code', 'build:code' ], () => {
     gulp.start('start');
-    gulp.watch([ `${folderSrc}/**/*.ts`, '!./node_modules/**/*', `!${folderSrc}/**/*.spec.ts` ], () => {
-        runSequence('tslint:code', 'build:code', 'start');
-    });
-});
+    gulp.watch([ `${folderSrc}/**/*.ts`, '!./node_modules/**/*', `!${folderSrc}/**/*.spec.ts` ],
+        gulp.series('tslint:code', 'build:code', 'start'));
+}));
 
 let spawn = require('child_process').spawn;
 let node;
