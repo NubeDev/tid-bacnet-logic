241b475472bec2c42c8e3c47d6293635a4bebd12
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-07-12 17:51:03 +0300

    add: Confirmed req reader tests
    Add subscribe COV message with issueConf & lifetime flags test

--- src/layers/reader/apdu/confirmed-request.pdu.spec.ts
+++ src/layers/reader/apdu/confirmed-request.pdu.spec.ts
@@ -4,5 +4,22 @@ import { ConfirmedRequest } from './confirmed-request.pdu';
 
 import * as Interfaces from '../../../interfaces';
 
+import * as IOs from '../../../io';
+import { IO } from '../../..';
+
 describe('ConfirmedReqPDU', () => {
+    describe('readSubscribeCOV', () => {
+        let buf: Buffer;
+
+        it('should read COV request with issueConf & lifetime params', () => {
+            buf = Buffer.from('0005010509001c0140001a29003c000493e0', 'hex');
+            const reader = new IOs.Reader(buf);
+            const msg = ConfirmedRequest.readLayer(reader);
+            const subReq = msg.service as Interfaces.ConfirmedRequest.Service.SubscribeCOV;
+            //console.log(JSON.stringify(msg));
+            expect(subReq.issConfNotif.value).to.equal(false);
+            expect(subReq.lifetime.value).to.equal(300000);
+        });
+
+    });
 });
