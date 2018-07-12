c20ca713cc12af18c6c4b4ddd23821ac6c3b7b18
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-07-12 18:26:05 +0300

    add: subscribeCOV message parsing tests
    Message without issueConf & lifetime flags

--- src/layers/reader/apdu/confirmed-request.pdu.spec.ts
+++ src/layers/reader/apdu/confirmed-request.pdu.spec.ts
@@ -21,5 +21,15 @@ describe('ConfirmedReqPDU', () => {
             expect(subReq.lifetime.value).to.equal(300000);
         });
 
+        it('should read COV request without issueConf & lifetime params', () => {
+            buf = Buffer.from('0005010509001c0140001a', 'hex');
+            const reader = new IOs.Reader(buf);
+            const msg = ConfirmedRequest.readLayer(reader);
+            const subReq = msg.service as Interfaces.ConfirmedRequest.Service.SubscribeCOV;
+            // console.log(JSON.stringify(msg));
+            expect(subReq.issConfNotif).to.equal(null);
+            expect(subReq.lifetime).to.equal(null);
+        });
+
     });
 });
